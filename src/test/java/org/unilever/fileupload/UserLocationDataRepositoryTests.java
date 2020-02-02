0bfe25e949ffec2c8ff307fdbfb8851954984581
saketpuranik <saket@theweirdscience.com>
2020-02-02 01:22:22 +0100

    @saket88:Added Changes for saving the list

--- src/test/java/org/unilever/fileupload/UserLocationDataRepositoryTests.java
+++ src/test/java/org/unilever/fileupload/UserLocationDataRepositoryTests.java
@@ -9,12 +9,16 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.unilever.fileupload.domain.UserLocationDataEntity;
 import org.unilever.fileupload.repository.UserLocationDataRepository;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
+import static org.hamcrest.CoreMatchers.hasItems;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.not;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
@@ -24,15 +28,19 @@ public class UserLocationDataRepositoryTests {
 
     @Test
     public void shouldBeAbleToSaveUserLocationData(){
+
+        List<UserLocationDataEntity> userLocationDataEntities = new ArrayList<>();
         UserLocationDataEntity userLocationDataEntity=
                 new UserLocationDataEntity(1l,"Hans","Amsterdam","Starbucks","Coffee");
+        UserLocationDataEntity userLocationDataEntityAgain=
+                new UserLocationDataEntity(2l,"Zimmer","Den Haag","Mac Donals","Restaurant");
+        userLocationDataEntities.add(userLocationDataEntity);
+        userLocationDataEntities.add(userLocationDataEntityAgain);
 
-        userLocationDataRepository.save(userLocationDataEntity);
+        userLocationDataRepository.saveAll(userLocationDataEntities);
+        List<UserLocationDataEntity> userLocationDataEntityRetrieved = userLocationDataRepository.findAll();
 
-        Optional<UserLocationDataEntity> userLocationDataEntityRetrieved = userLocationDataRepository.findById(1l);
-
-        assertTrue(userLocationDataEntityRetrieved.isPresent());
-
-        assertThat(userLocationDataEntity,is(userLocationDataEntityRetrieved.get()));
+        assertFalse(userLocationDataEntityRetrieved.isEmpty());
+        assertEquals(userLocationDataEntities,userLocationDataEntityRetrieved);
     }
 }
